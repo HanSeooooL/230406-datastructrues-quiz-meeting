3d2eae08e5dedd19b0b65e09a82c84f8259ad4bf
HANSEOL <nothinonthere@icoud.com>
2023-04-06 22:38:26 +0900

    [UPDATE] ADT little

--- 230406 datastructrues quiz meeting/main.c
+++ 230406 datastructrues quiz meeting/main.c
@@ -6,9 +6,34 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+
+void error(char str[])
+{
+    printf("%s", str);
+    exit(1);
+}
+
+
+typedef int Element;
+#define MAX_QUEUE_SIZE 100
+int front, rear;
+
+Element data[MAX_QUEUE_SIZE] = { 0 };
+
+void init_queue(void) {front = rear = 0;}
+int get_front(void) {return data[front];}
+int is_empty(void) {return front == rear;}
+int is_full(void) {return front == (rear - 1) % MAX_QUEUE_SIZE;}
+
+
+int enqueue(int x)
+{
+    
+    data[rear] = x;
+    rear = (rear + 1)
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
     return 0;
 }
